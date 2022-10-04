e57c2abac6b811a27b8782e5f0f92a07f606677b
artembilyy <incomecars4u@gmail.com>
2022-10-04 04:03:51 +0300

    Completed UI to AuthScreen.

--- CloudMovies/CloudMovies/View/AuthorizationTextField.swift
+++ CloudMovies/CloudMovies/View/AuthorizationTextField.swift
@@ -7,10 +7,10 @@
 
 import UIKit
 
-class PasswordTextField: UIView, UITextFieldDelegate {
-    
-    let lockImageView = UIImageView(image: UIImage(systemName: "lock.fill"))
+final class PasswordTextField: UIView, UITextFieldDelegate {
+    //MARK: - UI
     let textField = UITextField()
+    let lockImageView = UIImageView(image: UIImage(systemName: "lock.fill"))
     let placeHolderText: String
     let eyeButton = UIButton(type: .custom)
     let dividerView = UIView()
@@ -18,17 +18,17 @@ class PasswordTextField: UIView, UITextFieldDelegate {
     
     init(placeHolderText: String) {
         self.placeHolderText = placeHolderText
-        
         super.init(frame: .zero)
+        setupUI()
         
-        style()
-        layout()
     }
-
-    
+    override func layoutSubviews() {
+        setupConstraints()
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     override var intrinsicContentSize: CGSize {
         return CGSize(width: UIView.noIntrinsicMetric, height: 60)
     }
@@ -36,9 +36,8 @@ class PasswordTextField: UIView, UITextFieldDelegate {
 
 extension PasswordTextField {
     
-    func style() {
+    func setupUI() {
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemOrange
         errorLabel.text = "Your password must meet the requirements below"
         lockImageView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -47,21 +46,18 @@ extension PasswordTextField {
         textField.placeholder = placeHolderText
         textField.delegate = self
         textField.keyboardType = .asciiCapable
-        textField.attributedPlaceholder = NSAttributedString(string:placeHolderText,
+        textField.attributedPlaceholder = NSAttributedString(string: placeHolderText,
                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.secondaryLabel])
-
         addSubview(textField)
         
         eyeButton.translatesAutoresizingMaskIntoConstraints = false
         eyeButton.setImage(UIImage(systemName: "eye.circle"), for: .normal)
         eyeButton.setImage(UIImage(systemName: "eye.slash.circle"), for: .selected)
         eyeButton.addTarget(self, action: #selector(togglePasswordView), for: .touchUpInside)
-
         addSubview(eyeButton)
         
         dividerView.translatesAutoresizingMaskIntoConstraints = false
         dividerView.backgroundColor = .separator
-
         addSubview(dividerView)
         
         errorLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -71,13 +67,11 @@ extension PasswordTextField {
         errorLabel.isHidden = false
         errorLabel.numberOfLines = 0
         errorLabel.lineBreakMode = .byWordWrapping
-
         addSubview(errorLabel)
     }
     
-    func layout() {
+    func setupConstraints() {
         addSubview(lockImageView)
-        
         //lock
         NSLayoutConstraint.activate([
             lockImageView.topAnchor.constraint(equalTo: topAnchor),
@@ -107,7 +101,7 @@ extension PasswordTextField {
             errorLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             errorLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
-        // CHCR
+        // Priority
         lockImageView.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
         textField.setContentHuggingPriority(UILayoutPriority.defaultLow, for: .horizontal)
         eyeButton.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
@@ -115,8 +109,19 @@ extension PasswordTextField {
 }
 
 extension PasswordTextField {
+    //MARK: - Toogle Eye
     @objc func togglePasswordView(_ sender: Any) {
         textField.isSecureTextEntry.toggle()
         eyeButton.isSelected.toggle()
+    }
+    //MARK: Password limit
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        
+        return updatedText.count <= 32
     }
 }
--- CloudMovies/CloudMovies/View/AuthorizationViewController.swift
+++ CloudMovies/CloudMovies/View/AuthorizationViewController.swift
@@ -9,239 +9,160 @@ import UIKit
 import Lottie
 
 class AuthorizationViewController: UIViewController {
-    
-    private lazy var welcomeLabel: UILabel = {
+    //MARK: UI Elements
+    private let welcomeLabel: UILabel = {
         let welcomeLabel = UILabel()
         welcomeLabel.text = "Welcome"
-        welcomeLabel.numberOfLines = 1
+        welcomeLabel.textColor = .white
         welcomeLabel.textAlignment = .left
         welcomeLabel.adjustsFontForContentSizeCategory = true
-        welcomeLabel.minimumContentSizeCategory = .accessibilityLarge
+        welcomeLabel.minimumContentSizeCategory = .accessibilityExtraExtraLarge
         welcomeLabel.font = UIFont.preferredFont(forTextStyle: .title3)
         return welcomeLabel
     }()
-    private lazy var instructionLabel: UILabel = {
+    private let instructionLabel: UILabel = {
         let instructionLabel = UILabel()
         instructionLabel.text = "Sign in to Continue"
+        instructionLabel.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         instructionLabel.numberOfLines = 1
         instructionLabel.textAlignment = .left
         instructionLabel.adjustsFontForContentSizeCategory = true
-        instructionLabel.minimumContentSizeCategory = .accessibilityMedium
+        instructionLabel.minimumContentSizeCategory = .accessibilityLarge
         instructionLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
         return instructionLabel
     }()
-    
-    private lazy var usernameTextField: UITextField = {
-        let usernameTextField = UITextField()
-        usernameTextField.backgroundColor = .systemOrange
-        usernameTextField.autocorrectionType = .no
-        usernameTextField.autocapitalizationType = .none
-        usernameTextField.attributedText = NSAttributedString(string: "username")
-        return usernameTextField
-    }()
-    private lazy var wrongUsernameLabel: UILabel = {
-        let wrongUsernameLabel = UILabel()
-        wrongUsernameLabel.alpha = 1 //make it invisiable
-        wrongUsernameLabel.textColor = .systemRed
-        wrongUsernameLabel.textAlignment = .left
-        wrongUsernameLabel.text = "wrong username"
-        return wrongUsernameLabel
-    }()
-    private lazy var passwordTexField: UITextField = {
-        let passwordTextField = UITextField()
-        passwordTextField.backgroundColor = .systemCyan
-        passwordTextField.autocorrectionType = .no
-        passwordTextField.autocapitalizationType = .none
-        passwordTextField.attributedText = NSAttributedString(string: "username")
-        return passwordTextField
-    }()
-    private lazy var wrongPasswordLabel: UILabel = {
-        let wrongPasswordLabel = UILabel()
-        wrongPasswordLabel.alpha = 1 //make it invisiable
-        wrongPasswordLabel.textColor = .systemRed
-        wrongPasswordLabel.textAlignment = .left
-        wrongPasswordLabel.text = "wrong password"
-        return wrongPasswordLabel
-    }()
-    private lazy var enterButton: UIButton = {
+    private let enterButton: UIButton = {
         let enterButton = UIButton(type: .system)
-        enterButton.backgroundColor = .systemGray
+        enterButton.backgroundColor = #colorLiteral(red: 0.3952207565, green: 0.460826695, blue: 0.7956546545, alpha: 1)
+        enterButton.tintColor = .white
+        //        enterButton.tintColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        enterButton.layer.cornerRadius = 18
+        enterButton.titleLabel?.font = .systemFont(ofSize: 24)
         enterButton.setTitle("Sign in", for: .normal)
         return enterButton
     }()
     private let registerButton: UIButton = {
         let registerButton = UIButton(type: .system)
-        registerButton.backgroundColor = .systemPink
+        registerButton.titleLabel?.font = .systemFont(ofSize: 14)
         registerButton.setTitle("Sign up", for: .normal)
         return registerButton
     }()
     private lazy var forgetPasswordButton: UIButton = {
         let forgetPasswordButton = UIButton(type: .system)
-        forgetPasswordButton.setTitle("Forget password", for: .normal)
+        forgetPasswordButton.titleLabel?.font = .systemFont(ofSize: 14)
+        forgetPasswordButton.tintColor = .white
+        forgetPasswordButton.setTitle("Forget password?", for: .normal)
         return forgetPasswordButton
     }()
+    private lazy var backgroundAnimatiom: AnimationView = {
+        let background = AnimationView.init(name: "background")
+        background.contentMode = .scaleAspectFill
+        background.animationSpeed = 1
+        background.loopMode = .loop
+        background.play()
+        return background
+    }()
     
-    private let backgroundAnimatiom = AnimationView.init(name: "background")
-    private let newPasswordTextField = PasswordTextField(placeHolderText: "New password")
-    private let stackView = UIStackView()
+    private let passwordTextField = PasswordTextField(placeHolderText: "New password")
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        setupDismissKeyboardGesture()
+        setupKeyboardHiding()
     }
     override func viewWillLayoutSubviews() {
         setupConstraints()
-        style()
-        layout()
     }
-    
-    private func setupUI() {
-        backgroundAnimatiom.animationSpeed = 0.5
-        backgroundAnimatiom.loopMode = .loop
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         backgroundAnimatiom.play()
+    }
+}
+
+//MARK: - Setup UI & layout
+extension AuthorizationViewController {
+    private func setupUI() {
+        backgroundAnimatiom.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(backgroundAnimatiom)
-        view.addSubview(welcomeLabel)
-        view.addSubview(instructionLabel)
-        view.addSubview(usernameTextField)
-        view.addSubview(wrongUsernameLabel)
-        view.addSubview(passwordTexField)
-        view.addSubview(wrongPasswordLabel)
-        view.addSubview(enterButton)
-        view.addSubview(registerButton)
-        setupKeyboardHiding()
-    }
-    
-    private func setupConstraints() {
-        backgroundAnimatiom.frame = view.bounds
-        setupWelcomeLabel()
-        setupInstructionLabel()
-        setupUsernameTextField()
-        setupWrondUsernameLabel()
-        setupPasswordTexField()
-        setupWrondPasswordLabel()
-        setupEnterButton()
-        setutpRegisterButton()
-    }
-    
-    private func setupWelcomeLabel() {
         welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 220),
-            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
-        ])
-    }
-    
-    private func setupInstructionLabel() {
+        view.addSubview(welcomeLabel)
         instructionLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            instructionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            instructionLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 8),
-            instructionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
-        ])
-    }
-    
-    private func setupUsernameTextField() {
-        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            usernameTextField.topAnchor.constraint(equalTo: instructionLabel.bottomAnchor, constant: 40),
-            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
-        ])
-    }
-    
-    private func setupWrondUsernameLabel() {
-        wrongUsernameLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            wrongUsernameLabel.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 4),
-            wrongUsernameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            wrongPasswordLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
-    }
-    
-    private func setupPasswordTexField() {
-        passwordTexField.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            passwordTexField.topAnchor.constraint(equalTo: wrongUsernameLabel.bottomAnchor, constant: 40),
-            passwordTexField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            passwordTexField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            //            passwordTexField.widthAnchor.constraint(equalToConstant: 400),
-            passwordTexField.heightAnchor.constraint(equalToConstant: 50)
-        ])
-    }
-    
-    private func setupWrondPasswordLabel() {
-        wrongPasswordLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            wrongPasswordLabel.topAnchor.constraint(equalTo: passwordTexField.bottomAnchor, constant: 4),
-            wrongPasswordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            wrongPasswordLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
-    }
-    
-    private func setupEnterButton() {
+        view.addSubview(instructionLabel)
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        //        view.addSubview(passwordTextField)
         enterButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            enterButton.topAnchor.constraint(equalTo: wrongPasswordLabel.bottomAnchor, constant: 40),
-            enterButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 56),
-            enterButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -56),
-            enterButton.heightAnchor.constraint(equalToConstant: 100)
-        ])
+        view.addSubview(enterButton)
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(registerButton)
+        forgetPasswordButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(forgetPasswordButton)
     }
     
-    private func setutpRegisterButton() {
-        registerButton.translatesAutoresizingMaskIntoConstraints = false
+    func setupConstraints() {
+        //background
         NSLayoutConstraint.activate([
-            registerButton.topAnchor.constraint(equalTo: enterButton.bottomAnchor, constant: 32),
-            registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 56),
-            registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -56),
-            registerButton.heightAnchor.constraint(equalToConstant: 100)
+            backgroundAnimatiom.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundAnimatiom.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundAnimatiom.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundAnimatiom.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        //welcome
+        NSLayoutConstraint.activate([
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 160)
+        ])
+        //next label
+        NSLayoutConstraint.activate([
+            instructionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            instructionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            instructionLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 4)
+        ])
+        
+        //        NSLayoutConstraint.activate([
+        //            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        //            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        //            passwordTextField.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        //
+        //        ])
+        //sign in button
+        NSLayoutConstraint.activate([
+            enterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            enterButton.heightAnchor.constraint(equalTo: welcomeLabel.heightAnchor, multiplier: 1),
+            enterButton.widthAnchor.constraint(equalToConstant: view.frame.width / 1.3),
+            enterButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -view.frame.height / 4)
+        ])
+        //sign out button
+        NSLayoutConstraint.activate([
+            registerButton.topAnchor.constraint(equalTo: enterButton.bottomAnchor, constant: 8),
+            registerButton.trailingAnchor.constraint(equalTo: enterButton.trailingAnchor)
+            //            registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        //forger password
+        NSLayoutConstraint.activate([
+            forgetPasswordButton.topAnchor.constraint(equalTo: enterButton.bottomAnchor, constant: 8),
+            forgetPasswordButton.leadingAnchor.constraint(equalTo: enterButton.leadingAnchor)
         ])
     }
+    //MARK: - Keyboard Apperance Setup
+    private func setupDismissKeyboardGesture() {
+        let dismissKeyboardTap = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_: )))
+        view.addGestureRecognizer(dismissKeyboardTap)
+    }
+    
     private func setupKeyboardHiding() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-}
-
-
-extension AuthorizationViewController {
     
-    func style() {
-        newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 20
-    }
-    
-    func layout() {
-        stackView.addArrangedSubview(newPasswordTextField)
-        
-        view.addSubview(stackView)
-        
-        NSLayoutConstraint.activate([
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2)
-        ])
-        
-        view.addSubview(newPasswordTextField)
-        
-        NSLayoutConstraint.activate([
-            newPasswordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            newPasswordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
-        
-        NSLayoutConstraint.activate([
-            newPasswordTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: newPasswordTextField.trailingAnchor, multiplier: 1),
-            newPasswordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
-        
+    @objc func viewTapped(_ recognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
     }
     
     @objc func keyboardWillShow(sender: NSNotification) {
-        view.frame.origin.y = view.frame.origin.y - 200
+        view.frame.origin.y = view.frame.origin.y - view.frame.height / 4
     }
     
     @objc func keyboardWillHide(notification: NSNotification) {
